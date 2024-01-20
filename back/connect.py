9eaa8414442115f145daa207c094fad40c53d864
ericabuchanan <erica4@student.ubc.ca>
2024-01-20 14:08:09 -0800

    updated connect to not explicitly state password

--- back/connect.py
+++ back/connect.py
@@ -1,19 +1,13 @@
 import pymysql
-
-# Set the database credentials
-host = 'bdec.cjumg64mkato.us-east-1.rds.amazonaws.com'
-port = 3306
-user = 'admin'
-password = 'nwhacks2024'
-database = 'planner'
+import config
 
 # Connect to the database
 connection = pymysql.connect(
-    host=host,
-    port=port,
-    user=user,
-    password=password,
-    database=database  # could also be planner
+    host=config.host,
+    port=config.port,
+    user=config.user,
+    password=config.password,
+    database=config.database  # could also be planner
 )
 
 # Create a cursor object
