d2c8ff8f5a00f853e2e5aa603de22131a69da7ae
ericabuchanan <erica4@student.ubc.ca>
2024-01-20 13:53:53 -0800

    mysql connection set up complete

--- back/connect.py
+++ back/connect.py
@@ -1,34 +1,34 @@
 import pymysql
 
 # Set the database credentials
-host = '<YOUR_RDS_ENDPOINT>'
+host = 'bdec.cjumg64mkato.us-east-1.rds.amazonaws.com'
 port = 3306
-user = '<YOUR_DATABASE_USERNAME>'
-password = '<YOUR_DATABASE_PASSWORD>'
-database = '<YOUR_DATABASE_NAME>'
+user = 'admin'
+password = 'nwhacks2024'
+database = 'planner'
 
 # Connect to the database
 connection = pymysql.connect(
-    host="bdec.cjumg64mkato.us-east-1.rds.amazonaws.com",
-    port=3306,
-    user="nwhacks2024",
-    password="nwhacks2024",
-    database="bdec"  # could also be planner
+    host=host,
+    port=port,
+    user=user,
+    password=password,
+    database=database  # could also be planner
 )
 
 # Create a cursor object
-# cursor = connection.cursor()
-#
-# # Execute a SQL query
-# cursor.execute('SELECT * FROM users')
-#
-# # Fetch the results
-# results = cursor.fetchall()
-#
-# # Print the results
-# for result in results:
-#     print(result)
-#
-# # Close the cursor and connection
-# cursor.close()
+cursor = connection.cursor()
+
+# Execute a SQL query
+cursor.execute('SELECT * FROM Degrees')
+
+# Fetch the results
+results = cursor.fetchall()
+
+# Print the results
+for result in results:
+    print(result)
+
+# Close the cursor and connection
+cursor.close()
 connection.close()
