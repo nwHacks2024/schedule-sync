d187298a30e1ab4a7f27ae8b467005a2b4054a88
davenfroberg <davenfroberg@gmail.com>
2024-01-20 17:08:59 -0800

    First endpoint

--- back/api.py
+++ back/api.py
@@ -1,13 +1,34 @@
-from flask import Flask
+import json
+
+from flask import Flask, request, jsonify
 import connect
 
 app = Flask(__name__)
 
 
-@app.route('/api/users', methods=['GET'])
-def hello():
-    results = connect.query("SELECT * FROM Students")
-    return {'message': 'Hello, World!'}
+@app.route('/api/userinfo', methods=['GET'])
+def userinfo():
+    titles = connect.query("SHOW COLUMNS FROM Students")
+    data = request.get_json()
+    if 'username' not in data:
+        return jsonify({'error': 'Missing username field'}), 400
+
+    username = data['username']
+    results = connect.query(f"SELECT * FROM Students WHERE username = '{username}'")
+    keys = []
+    for title in titles:
+        if title[0] != 'hashedPassword' and title[0] != 'salt':
+            keys.append(title[0])
+
+    values = []
+    for entry in results[0]:
+        values.append(entry)
+
+    data_dict = dict(zip(keys, values))
+
+    # Convert the dictionary to a JSON-formatted string
+    json_data = json.dumps(data_dict)
+    return json_data
 
 if __name__ == '__main__':
     app.run(debug=True)
