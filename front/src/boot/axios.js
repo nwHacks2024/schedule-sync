1e6514eb358a4a2f2b729f75e07491c2802520cb
Carlos <carrlosaperrez@gmail.com>
2024-01-20 20:41:28 -0800

    fixed api

--- front/src/boot/axios.js
+++ front/src/boot/axios.js
@@ -1,11 +1,6 @@
 import { boot } from 'quasar/wrappers'
 import axios from 'axios'
 
-<<<<<<< Updated upstream
-// const api = axios.create({ baseURL: 'http://localhost:6543' })
-=======
-// const api = axios.create({ baseURL: 'https://catfact.ninja' })
->>>>>>> Stashed changes
 // "async" is optional;
 // more info on params: https://v2.quasar.dev/quasar-cli/boot-files
 export default boot(({ app }) => {
