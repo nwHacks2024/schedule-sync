dab0ec449a947390a18ed88421b4709def0bde86
Carlos <carrlosaperrez@gmail.com>
2024-01-21 02:26:54 -0800

    YAY DONE ME

--- front/src/components/CalendarComponent.vue
+++ front/src/components/CalendarComponent.vue
@@ -4,6 +4,7 @@
     :time-to="23 * 60"
     events-count-on-year-view
     :events="events"
+    style="height: 600px"
   />
 </template>
 
@@ -26,26 +27,27 @@ export default defineComponent({
   methods: {
     async fetchSections() {
       try {
-        const response = await axios.post('/api/registeredcourses', {
-          username: 'dfroberg',
+        const response = await axios.post("/api/registeredcourses", {
+          username: "dfroberg",
         });
         const sections = response.data.results;
-        console.log(sections)
+        console.log(sections);
 
         // Process API response and convert it into events format
-        this.events = sections.flatMap(section => {
+        this.events = sections.flatMap((section) => {
           // Assuming each section has an 'events' key with a list of event dates
           const eventDates = section.events || [];
 
-          return eventDates.map(date => ({
+          return eventDates.map((date) => ({
             start: `${date} ${section.startTime}`,
             end: `${date} 16:00`, // You need to define endTime in your API response
             title: section.courseName,
+            class: 'me'
           }));
         });
 
-        console.log('finished processing')
-        console.log(this.events)
+        console.log("finished processing");
+        console.log(this.events);
       } catch (error) {
         console.error("Error fetching sections:", error);
       }
@@ -53,3 +55,10 @@ export default defineComponent({
   },
 });
 </script>
+<style>
+.vuecal__event.me {
+  background-color: rgba(253, 156, 66, 0.9);
+  border: 1px solid rgb(233, 136, 46);
+  color: #fff;
+}
+</style>
